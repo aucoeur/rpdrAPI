22a1eaa5e7030fce557c7735077550c06a9a1958
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-03-02 16:14:51 -0800

    Auth with headers fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,37 +2,41 @@ const User = require("../models/user");
 const jwt = require('jsonwebtoken');
 
 //Check auth - headers
-// let checkAuth = (req, res, next) => {
-//   console.log("Checking authentication");
-//   if (typeof req.headers.jwtToken === "undefined" || req.headers.jwtToken === null) {
-//     req.user = null;
-//     next();
-//   } else {
-//     let token = req.headers.jwtToken;
-//     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-//       if (err) {
-//           console.log('Error during authentication: Invalid signature')
-//           req.user = null;
-//       } else {
-//           req.user = decodedToken;
-//       }
-//       next();
-//     })
-//   }
-// };
+let checkAuth = (req, res, next) => {
+  const authorization = req.headers['authorization']
+  console.log("Checking authentication");
+  console.log(authorization)
+
+  if (typeof req.headers.authorization === "undefined" || req.headers.authorization === null) {
+    req.user = null;
+    next();
+  } else {
+    const bearer = authorization.split(' ');
+    let token = bearer[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+          console.log('Error during authentication: Invalid signature')
+          req.user = null;
+      } else {
+          req.user = decodedToken;
+      }
+      next();
+    })
+  }
+};
 
 // Check auth - cookies
-const checkAuth = (req, res, next) => {
-  console.log("Checking authentication")
-  if (typeof req.cookies.jwtToken === "undefined" || req.cookies.jwtToken === null) {
-    console.log(req.cookies.jwtToken)
-    req.user = null;
-  } else {
-    const token = req.cookies.jwtToken;
-    const decodedToken = jwt.decode(token, { complete: true }) || {};
-    req.user = decodedToken.payload;
-  }
-  next();
-};  
+// const checkAuth = (req, res, next) => {
+//   console.log("Checking authentication")
+//   if (typeof req.cookies.jwtToken === "undefined" || req.cookies.jwtToken === null) {
+//     console.log(req.cookies.jwtToken)
+//     req.user = null;
+//   } else {
+//     const token = req.cookies.jwtToken;
+//     const decodedToken = jwt.decode(token, { complete: true }) || {};
+//     req.user = decodedToken.payload;
+//   }
+//   next();
+// };  
 
 module.exports = checkAuth
